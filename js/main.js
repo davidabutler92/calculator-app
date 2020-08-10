13812e86b2396db9c1648bdf3f0b132e58f1678d
davidabutler92 <davidabutler92@yahoo.com>
2020-08-10 13:46:23 -0700

    add multiply and division functions.
    Co-authored-by Drew Lockhart <drewmlockhart@gmail.com>

--- js/main.js
+++ js/main.js
@@ -10,10 +10,13 @@ function multiply(number1, number2) {
   return number1 * number2;
 }
 
+function division(number1, number2) {
+    return number1 / number2
+}
 
 
 const number1 = parseInt(prompt("Enter a number:"));
 const number2 = parseInt(prompt("Enter another number"));
 
-alert(subtract(number1, number2));
+alert(division(number1, number2));
 
